89b6b774995e11eb36edd10d930fba445826d13b
keyboard-warrior1 <65595515+keyboard-warrior1@users.noreply.github.com>
2022-12-09 22:41:37 +0200

    Update AppDelegate.swift

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -10,9 +10,9 @@ import google_mobile_ads
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyBC0Waje9kAnLXpGRm5odsPWgXW_5t59sk")
     GeneratedPluginRegistrant.register(with: self)
     FirebaseApp.configure()
-    GMSServices.provideAPIKey("AIzaSyBC0Waje9kAnLXpGRm5odsPWgXW_5t59sk")
     let nativeAdFactory = NativeAdFactoryExample()
     FLTGoogleMobileAdsPlugin.registerNativeAdFactory(
         self, factoryId: "adFactoryExample", nativeAdFactory: nativeAdFactory)
