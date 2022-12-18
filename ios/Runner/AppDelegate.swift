ffa66d05651b6a0899e61ef6ec5130ec7b4de9ad
keyboard-warrior1 <65595515+keyboard-warrior1@users.noreply.github.com>
2022-12-19 01:20:41 +0200

    Update AppDelegate.swift

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -16,5 +16,28 @@ import google_mobile_ads
         self, factoryId: "adFactoryExample", nativeAdFactory: nativeAdFactory)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+ 
+  class NativeAdFactoryExample : FLTNativeAdFactory {
+
+    func createNativeAd(_ nativeAd: GADNativeAd,
+                        customOptions: [AnyHashable : Any]? = nil) -> GADNativeAdView? {
+        let nibView = Bundle.main.loadNibNamed("NativeAdView", owner: nil, options: nil)!.first
+        let nativeAdView = nibView as! GADNativeAdView
+
+        (nativeAdView.headlineView as! UILabel).text = nativeAd.headline
+
+        (nativeAdView.bodyView as! UILabel).text = nativeAd.body
+        nativeAdView.bodyView!.isHidden = nativeAd.body == nil
+
+        (nativeAdView.iconView as! UIImageView).image = nativeAd.icon?.image
+        nativeAdView.iconView!.isHidden = nativeAd.icon == nil
+
+        nativeAdView.callToActionView?.isUserInteractionEnabled = false
+
+        nativeAdView.nativeAd = nativeAd
+
+        return nativeAdView
+    }
+}
 }
 
